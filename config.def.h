b0b0efeb635d6a9463d35138e07b70ab09166ee3
Torskaren <101107437+Torskaren@users.noreply.github.com>
2022-03-10 23:58:22 -0500

    Add files via upload

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#18b53a";
-static const char col_pink[]        = "#3485d1";
+static const char col_pink[]        = "#3D92E0";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_pink  },
@@ -20,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "0", "1", "2", "3", "4" };
+static const char *tags[] = { "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
